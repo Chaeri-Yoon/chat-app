8831276b5aae77cdb4d3169d6243572266d5fb5d
Chaeri Yoon <cryoon0103@gmail.com>
2022-05-10 23:55:19 +0900

    * Plan Basic Structure * Socket Server connect

---
+++ server/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+const httpServer = createServer(app);
+const server = new Server(httpServer);
+server.on("connection", socket => {
+    console.log(`${socket.id} is connected`);
+    socket.on("disconnect", () => console.log(`${socket.id} is disconnected`));
+})
+
+httpServer.listen(process.env.SERVER_PORT | 3000, () => console.log(`Server is running on Port ${process.env.SERVER_PORT | 3000}`));

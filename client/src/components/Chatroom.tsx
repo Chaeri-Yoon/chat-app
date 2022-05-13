e7b79f91638254aa9b4dc4a36f56a7567ebd2959
Chaeri Yoon <cryoon0103@gmail.com>
2022-05-13 23:39:02 +0900

    * If user try to enter the chatroom through url, not after entering nickname and choosing avatar, the browser will direct them to homepage again.

--- client/src/components/Chatroom.tsx
+++ client/src/components/Chatroom.tsx
@@ -1,10 +1,13 @@
-import React from "react"
-import { useLocation } from "react-router-dom";
+import React, { useEffect } from "react"
+import { useLocation, useNavigate } from "react-router-dom";
 import { IEnterForm } from '../interface';
 
+
 export default () => {
+    const navigate = useNavigate();
     const location = useLocation();
-    const { nickname, avatarNum } = location.state as IEnterForm;
+    const { nickname, avatarNum } = useLocation()?.state as IEnterForm || { nickname: '', avatarNum: '' };
+    useEffect(() => { if (nickname === '' || avatarNum.toString() === '') navigate('/'); }, [location]);
 
     return (
         <div>Chatroom</div>
